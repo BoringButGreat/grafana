d0dbd41e6a66ad6bf4a8f0d15439c25383cdbb32
Peter B <peter.bonney@gmail.com>
2016-05-12 14:23:47 -0400

    starting to build cookie support

--- lib/grafana.ex
+++ lib/grafana.ex
@@ -7,7 +7,8 @@ defmodule Grafana do
   @username Application.get_env(:grafana, :username)
   @password Application.get_env(:grafana, :password)
 
-  @headers ["Authorization": @api_key]
+  @auth_header ["Authorization": @api_key]
+  @json_header ["Content-Type": "application/json"]
 
   @docmodule """
   Tools for getting data from the Grafana API as well as chart objects for
@@ -15,35 +16,28 @@ defmodule Grafana do
   """
 
   def process_url(suffix) do
-    IO.inspect(suffix)
     @api_host <> suffix
   end
 
+  def process_request_headers(headers) do
+    IO.inspect(headers++@json_header++["Cookie": get_cookie])
+    headers++@json_header++["Cookie": get_cookie]
+  end
+
   def set_cookie(cookie) do
-
+    cookie_jar = parse_cookie_list(cookie)
+    max_age = get_max_age(cookie)
+    %{cookie: cookie_jar, }
   end
 
-  def get_cookie do
+  def parse_cookie_list([]), do: ""
+  def parse_cookie_list([head | tail]), do: "#{hd(String.split(head, "; "))};#{parse_cookie_list(tail)}"
+  def parse_cookie_list(_), do: ""
+
+  def process_response_body(body) do
+    {:ok, result} = body |> IO.iodata_to_binary |> Poison.decode
+    result |> Enum.map(fn ({key, val}) -> { String.to_atom(key), val } end)
+    |> :orddict.from_list
   end
-
-  # def process_options(options) do
-  #   # IO.inspect(options++[basic_auth: {"#{@username}", "#{@password}"}])
-  #   # options++[basic_auth: {"#{@username}", "#{@password}"}]
-  #   IO.inspect(options)
-  #   IO.inspect(get_cookie)
-  #   options++["Cookie": get_cookie]
-  # end
-
-  def handle_response(response) do
-    response
-  end
-
-  # def process_response_body(body) do
-  #   IO.inspect(body)
-  #   {:ok, result} = body |> IO.iodata_to_binary |> Poison.decode
-  #   IO.inspect(result)
-  #   result |> Enum.map(fn ({key, val}) -> { String.to_atom(key), val } end)
-  #   |> :orddict.from_list
-  # end
 
 end
--- lib/panel/embed.ex
+++ lib/panel/embed.ex
@@ -4,13 +4,13 @@ defmodule Grafana.Panel.Embed do
   @password Application.get_env(:grafana, :password)
 
   # model
-  # <iframe src="http://xxx.xxx.xxx.xxx:3000/dashboard-solo/db/worldping-endpoint-ping?panelId=588&from=1462358038345&to=1462368778346&var-endpoint=boringbutgreat_com&var-probe=All&var-outliers=100" width="450" height="200" frameborder="0"></iframe>
-  defp build_src(uri, panel_id, from, to, endpoint, probe, outliers) do
-    "#{@api_host}/dashboard-solo/#{uri}?panelId=#{panel_id}&from=#{from}&to=#{to}&var-endpoint=#{endpoint}&var-probe=#{probe}&var-outliers=#{outliers}"
+  # <iframe src="http://xxx.xxx.xxx.xxx:3000/dashboard-solo/db/worldping-endpoint-ping?panel_id=1" width="450" height="200" frameborder="0"></iframe>
+  defp build_src(uri, panel_id) do
+    "#{@api_host}/dashboard-solo/db/#{uri}?panelId=#{panel_id}"
   end
 
-  def get_html(uri, endpoint, from, to, width, height, border) do
-    src = build_src uri, "588", from, to, endpoint, "All", "100"
+  def get_html(uri, panel_id, width, height, border) do
+    src = build_src uri, panel_id
     "<iframe src=\"#{src}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"#{border}\"></iframe>"
   end
 end
