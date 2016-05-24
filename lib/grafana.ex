e394ac8be9bf8a9edca7b8a26008add1368a7717
Peter B <peter.bonney@gmail.com>
2016-05-24 11:19:38 -0400

    small change to structure

--- lib/grafana.ex
+++ lib/grafana.ex
@@ -1,21 +1,27 @@
 defmodule Grafana.Base do
-  use Grafana.API
   use HTTPotion.Base
+    @docmodule """
+    When used, it defines overridable functions that allow you to make custom
+    Grafana clients.
+    """
+
+    @api_host Application.get_env(:grafana, :api_host)
+    @api_key Application.get_env(:grafana, :api_key)
+
+    @doc """
+    Prepend URL of API host, so that e.g. Grafana.post can be called with just a
+    URL suffix: Grafana.post("/login") => HTTPotion.post("<api_host url>/login")
+    """
+    def process_url(suffix) do
+      @api_host <> suffix
+    end
+end
+
+defmodule Grafana do
+  use Grafana.Base
 
   @docmodule """
   Tools for getting data from the Grafana API as well as chart objects for
   embedding.
   """
-
-  @api_host Application.get_env(:grafana, :api_host)
-  @api_key Application.get_env(:grafana, :api_key)
-
-  @doc """
-  Prepend URL of API host, so that e.g. Grafana.post can be called with just a
-  URL suffix: Grafana.post("/login") => HTTPotion.post("<api_host url>/login")
-  """
-  def process_url(suffix) do
-    @api_host <> suffix
-  end
-
 end
