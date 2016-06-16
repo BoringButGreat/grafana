5dcd3e808c799793ce3cf106097fd34c80be302a
Peter B <peter.bonney@gmail.com>
2016-06-16 16:36:23 -0400

    killing @ refs - leaving to user to define

--- lib/grafana.ex
+++ lib/grafana.ex
@@ -7,17 +7,6 @@ defmodule Grafana.Base do
   defmacro __using__(_) do
     quote do
       use HTTPotion.Base
-
-      @api_host Application.get_env(:grafana, :api_host)
-      @api_key Application.get_env(:grafana, :api_key)
-
-      @doc """
-      Prepend URL of API host, so that e.g. Grafana.post can be called with just a
-      URL suffix: Grafana.post("/login") => HTTPotion.post("<api_host url>/login")
-      """
-      def process_url(suffix) do
-        @api_host <> suffix
-      end
     end
   end
 end
