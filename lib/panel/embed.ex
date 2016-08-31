3a5cdfcbb6a98703b992908c5b844ce9177afffa
Peter B <peter.bonney@gmail.com>
2016-08-31 14:02:31 -0400

    change compile-time variables to run-time

--- lib/panel/embed.ex
+++ lib/panel/embed.ex
@@ -1,12 +1,12 @@
 defmodule Grafana.Panel.Embed do
-  @api_host Application.get_env(:grafana, :api_host)
-  @username Application.get_env(:grafana, :username)
-  @password Application.get_env(:grafana, :password)
+  def api_host, do: Application.get_env(:grafana, :api_host)
+  def username, do: Application.get_env(:grafana, :username)
+  def password, do: Application.get_env(:grafana, :password)
 
   # model
   # <iframe src="http://xxx.xxx.xxx.xxx:3000/dashboard-solo/db/worldping-endpoint-ping?panel_id=1" width="450" height="200" frameborder="0"></iframe>
   defp build_src(uri, panel_id) do
-    "#{@api_host}/dashboard-solo/db/#{uri}?panelId=#{panel_id}"
+    "#{api_host}/dashboard-solo/db/#{uri}?panelId=#{panel_id}"
   end
 
   def get_html(uri, panel_id, width, height, border) do
