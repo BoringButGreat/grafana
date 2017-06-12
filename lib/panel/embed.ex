f05908587f8912cb7f13955ec4cd1693b286245a
Toyah <toyah@tag-ip.com>
2017-06-12 17:01:25 +0300

    feat: add refresh

--- lib/panel/embed.ex
+++ lib/panel/embed.ex
@@ -5,13 +5,27 @@ defmodule Grafana.Panel.Embed do
 
   # model
   # <iframe src="http://xxx.xxx.xxx.xxx:3000/dashboard-solo/db/worldping-endpoint-ping?panel_id=1" width="450" height="200" frameborder="0"></iframe>
+  defp build_src(uri, panel_id, refresh) do
+    "#{api_host()}/dashboard-solo/db/#{uri}?panelId=#{panel_id}&refresh=#{refresh}"
+  end
+
   defp build_src(uri, panel_id) do
     "#{api_host()}/dashboard-solo/db/#{uri}?panelId=#{panel_id}"
+  end
+
+  def get_html(uri, panel_id, width, height, border, refresh) do
+    src = build_src uri, panel_id, refresh
+    "<iframe src=\"#{src}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"#{border}\"></iframe>"
   end
 
   def get_html(uri, panel_id, width, height, border) do
     src = build_src uri, panel_id
     "<iframe src=\"#{src}\" width=\"#{width}\" height=\"#{height}\" frameborder=\"#{border}\"></iframe>"
+  end
+
+  def model(uri, panel_id, width, height, border, refresh) do
+    src = build_src uri, panel_id, refresh
+    %{src: src, width: width, height: height, frameborder: border, refresh: refresh}
   end
 
   def model(uri, panel_id, width, height, border) do
